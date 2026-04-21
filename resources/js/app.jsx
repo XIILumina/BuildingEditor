8d05cea56e9f7291cc7c525e4d4cbccddea60f12
XIILumina <zibrevs@inbox.lv>
2026-04-21 14:14:36 +0300

    forgot to commit

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,21 @@
 import '../css/app.css';
 import './bootstrap';
 
+import axios from 'axios';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
+// CSRF setup
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const csrfToken = document.head.querySelector('meta[name="csrf-token"]');
+if (csrfToken) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken.content;
+}
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-const title = import.meta.env.VITE_TITLE || 'Laravel';
+
 
 createInertiaApp({
     title: (title) => `${title} ${appName}`,
